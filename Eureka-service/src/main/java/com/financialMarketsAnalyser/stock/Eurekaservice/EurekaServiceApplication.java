e03b520a8e62d25389a3660e8eddd5cb145e022b
vzehraz <98289184+vzehraz@users.noreply.github.com>
2023-02-27 23:59:13 +0000

    Add files via upload

---
+++ Eureka-service/src/main/java/com/financialMarketsAnalyser/stock/Eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,17 @@
+package com.financialMarketsAnalyser.stock.Eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+}
+
---
+++ Eureka-service/src/test/java/com/financialMarketsAnalyser/stock/Eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialMarketsAnalyser.stock.Eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
